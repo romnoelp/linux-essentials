fe7a464ad797abab63df60db1a4f4bbdc11ac212
Romnoel Edralin Petracorta <petracorta.rmnl@gmail.com>
2025-02-24 05:53:04 +0000

    Conditionals in bash.

--- conditionals.sh
+++ conditionals.sh
@@ -1,22 +1,26 @@
-echo "Enter your exam score (0-100):"
-read score
+echo "Exam score:"
+read examScore
 
-if ! [[ "$score" =~ ^[0-9]+$ ]]; then
+
+if ! [[ "$examScore" =~ ^[0-9]+$ ]]; then
     echo "Error: Input is not a valid number."
     exit 1
 fi
 
-if (( score < 0 || score > 100 )); then
+
+if (( $examScore < 0 || $examScore > 100 )); then
     echo "Error: Score must be between 0 and 100."
     exit 1
 fi
 
-case $score in
-    [9][0-9] | 100) grade="A" ;; 
-    [8][0-9]) grade="B" ;;       
-    [7][0-9]) grade="C" ;;       
-    [6][0-9]) grade="D" ;;       
+
+case $examScore in
+    [9][0-9] | 100) grade="A" ;;
+    [8][0-9]) grade="B" ;;      
+    [7][0-9]) grade="C" ;;      
+    [6][0-9]) grade="D" ;;      
     *) grade="F" ;;              
 esac
+
 
 echo "Grade $grade"
