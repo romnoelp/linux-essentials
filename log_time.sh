d3c7a352b469d4b0536c0082948c3bbdc693b4e7
Romnoel Edralin Petracorta <petracorta.rmnl@gmail.com>
2025-04-07 05:22:38 +0000

    Edited the log sh file

--- log_time.sh
+++ log_time.sh
@@ -1,1 +1,12 @@
-echo "Current Date and Time: $(date)">>log.txt
+
+current_time=$(date "+%Y-%m-%d %H:%M:%S")
+
+
+github_username=$GITHUB_ACTOR
+
+if [ ! -f log.txt ]; then
+  echo "Log file created at $current_time by $github_username" > log.txt
+else
+  update_count=$(wc -l < log.txt)
+  echo "Log update #$update_count: $current_time by $github_username" >> log.txt
+fi
